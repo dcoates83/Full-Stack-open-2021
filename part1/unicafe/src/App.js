4f1843dd4b861e06cdc975e9e4629d0018dd35d2
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-05-01 19:35:01 -0600

    Finished exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,58 @@
 import React, { useState } from 'react'
 
+const Button = (props)=>{
+  return <button onClick={props.handleClick}>{props.text}</button>
+}
+
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [clicks, setClicks] = useState({
+    good: 0, bad: 0, neutral: 0, total: 0, average:0
+  })
+  
+  const handleGood = () => {
+    const newClicks = {
+      ...clicks,
+      good: clicks.good + 1,
+      total: clicks.total + 1,
+      average: clicks.average + 1,
+    }
+    setClicks(newClicks)
+  }
+  const handleBad = () => {
+    const newClicks = {
+      ...clicks,
+      bad: clicks.bad + 1,
+      total: clicks.total + 1,
+      average: clicks.average - 1,
+    }
+    setClicks(newClicks)
+  }
+  const handleNeutral = () => {
+    const newClicks = {
+      ...clicks,
+      neutral: clicks.neutral + 1,
+      total: clicks.total +1,
+    }
+    setClicks(newClicks)
+  }
   return (
     <div>
       <h1>Give Feedback</h1>
       
-      <button onClick={() => {
-      setGood(good + 1)
-      }}>Good</button>
-      <button onClick={() => {
-      setNeutral(neutral + 1)
-      }}>Neutral</button>
-      <button onClick={() => {
-      setBad(bad + 1)
-      }}>Bad</button>
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
     
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <p>Good {clicks.good}</p>
+      <p>Neutral {clicks.neutral}</p>
+      <p>Bad {clicks.bad}</p>
+      <p>Total {clicks.total}</p>
+      <p>Average {clicks.average / clicks.total}</p>
+      <p>Positive {clicks.good / clicks.total * 100}%</p>
+    
+      
 
     </div>
   )
