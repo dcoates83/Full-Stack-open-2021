a9580fcb303b1415f81a4bb08a1623e22329ca2b
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-05-11 13:30:44 -0600

    finished exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,36 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', id:0 }
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
   // const [showAll, setShowAll] = useState(true)
+  const filterNotes = (e) => {
+    const newFilter = {
+      name: filter
+    }
+    let nameFilter = persons.filter(person => person.name.includes(newFilter.name))
+    console.log(nameFilter);
+    
+    return (nameFilter.map(person => {
+      return (<div key={person.name}>
+        <p>Name: {person.name}</p>
+        <p>Phone: {person.phone}</p>
+        -----------
+      </div>)
+    }))
+  }
   const addNote = (event) => {
     event.preventDefault()
 
-    let check =  persons.find(person => person.name === newName ) 
+    let check = persons.find(person => person.name === newName)
+   
+    
     if (!check) {
       const noteObject = {
         name: newName,
@@ -32,6 +53,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input value={filter} onChange={(e) => {   setFilter(e.target.value) }}/>
+        </div>
+      <h3>Add a New</h3>
       <form onSubmit={addNote}>
         <div>
           Name: <input value={newName }onChange={(e) => {setNewName(e.target.value)} }/>
@@ -44,18 +69,21 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-     
+{/*      
       <div>
         {persons.map(person => {
           // console.log(person);
-         return(<div key={person.id}>
+         return(<div key={person.name}>
            <p>Name: {person.name} </p>
            <p>Phone: { person.phone} </p>
               </div>)
         })}
       </div>
       
-      <div>debug: {newName}</div>
+      <div>debug: {filter}</div> */}
+      <div>
+        {filterNotes()}
+      </div>
     </div>
   )
 }
